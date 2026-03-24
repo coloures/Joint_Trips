dbbb4a61773f542155decdfd9b9755b3b2522c9a
Gleb <glebbindjuk@gmail.com>
2026-03-24 14:00:20 +0600

    Card adding trip

---
+++ app/components/UI/CardAddingTrip.vue
@@ -0,0 +1,22 @@
+<template>
+  <GridLayout columns="*" class="bg-white rounded-2xl p-4 mb-3" @tap="onTap">
+    <Label col="0" :text="icon" class="text-4xl mr-3 text-center" />
+  </GridLayout>
+</template>
+
+<script>
+export default {
+  name: 'TravelCard',
+  props: {
+    icon: {
+      type: String,
+      default: '+'
+    }
+  },
+  methods: {
+    onTap() {
+      this.$emit('addNewTrip')
+    }
+  }
+}
+</script>
